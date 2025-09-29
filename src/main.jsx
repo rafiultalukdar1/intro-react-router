8c7cf658299792e3b24c783fee3b49243179aaad
rafi0012 <rafiultalukdar007@gmail.com>
2025-09-29 12:06:03 +0600

    Intro React Router

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Hello React Router</h1>
+  },
+]);
+
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
